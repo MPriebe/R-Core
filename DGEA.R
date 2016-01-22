508973b089196aab7a3ef71ed855af4f842eb8f0
Nazrath10R <nazrath.nawaz@yahoo.de>
2016-01-22 11:17:01 +0000

    added annotations

--- DGEA.R
+++ DGEA.R
@@ -6,7 +6,7 @@
 # ---------------------------------------------------------
 
 #############################################################################
-# Load necessary dependancies, if not previously installed            #
+#       Load necessary dependancies, if not previously installed            #
 #############################################################################
 
 # source('http://bioconductor.org/biocLite.R')
@@ -14,7 +14,7 @@
 # install.packages('rjson')
 
 #############################################################################
-#             Gene Expression  Analysis               #
+#                        Gene Expression  Analysis                          #
 #############################################################################
 
 library('Cairo')
@@ -30,22 +30,18 @@ library('reshape2')
 # Command Line Arguments
 working.dir   <- '/Users/nazrathnawaz/Dropbox/GroupPoject/RCore/CourseWorkScripts'
 accession.id  <- 'GDS5093' #  GDS5092 GDS5091 GDS5088 GDS5086 GDS3795
-working.dir   <- '/Users/nazrathnawaz/Dropbox/GroupPoject/RCore/CourseWorkScripts'
 factor_type   <- 'disease.state'
 #factor_type  <- 'genotype/variation'
 #factor_type  <- 'development.stage'
 #factor_type  <- 'infection'
 
 
-
 # import data sets and process into expression data
-gse            <- getGEO(accession.id, GSEMatrix = TRUE)  # Load the data
-eset           <- GDS2eSet(gse, do.log2=TRUE)       # Convert into ExpressionSet Object
-X              <- exprs(eset)               # Get Expression Data
-geneNames        <- as.character(gse@dataTable@table$IDENTIFIER)
+gse              <- getGEO(accession.id, GSEMatrix = TRUE)       # Load GEO data
+eset             <- GDS2eSet(gse, do.log2=TRUE)                  # Convert into ExpressionSet Object
+X                <- exprs(eset)                                  # Get Expression Data
+geneNames        <- as.character(gse@dataTable@table$IDENTIFIER) # Store gene names
 names(geneNames) <- rownames(X)
-
-str(pData(eset))
 
 # Create a data frame with the factors
 expression.info <- data.frame(pData(eset)[factor_type],
@@ -55,13 +51,14 @@ data <- within(melt(X), {
     Factors = expression.info[Var2, factor_type]
 })
 
-# BoxPlots with colours based on the disease states
+# BoxPlot with colours based on the factors given
 boxplot <- ggplot(data) + labs(x = 'Sample', y = 'Expression Levels') +
     theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = 'bottom'
     ) + geom_boxplot(aes(x = Var2, y = value, colour = Factors))
 
+# store Boxplot as an .svg file in the working directory
 filename <- paste(working.dir,"boxplot.svg",sep = "")
-ggsave(filename,plot=boxplot, width = 8, height = 4)
+ggsave(filename, plot=boxplot, width = 8, height = 4)
 
 
 ######
@@ -69,17 +66,16 @@ ggsave(filename,plot=boxplot, width = 8, height = 4)
 ######
 #which((pData(eset) == levels(pData(eset))[1]) == TRUE)
 
-pClass <- as.factor(pData(eset)[,factor_type])
-
-
+pClass        <- as.factor(pData(eset)[,factor_type])
 names(pClass) <- sampleNames(eset)
 
 # remove white spaces in factors and rename
-levels(pClass) <-unlist(lapply(levels(pClass),function (x) gsub(" ", "", x, fixed = TRUE)))
+levels(pClass) <-unlist(lapply(levels(pClass), function (x) gsub(" ", "", x, fixed = TRUE)))
 
 arr   <- levels(pClass)
 phase <- c()
 
+## to be replaced ##
 for(i in seq(1,length(arr)-1))
 {
     for(j in seq(i+1,length(arr)))
@@ -92,8 +88,12 @@ for(i in seq(1,length(arr)-1))
 }
 
 
+#### Using the limma package ####
+
 design  <- model.matrix(~0+pClass)
-fit   <- lmFit(X, design)
+
+# plots linear model for each gene and estimate fold changes and standard errors
+fit     <- lmFit(X, design)
 
 # set contrasts for all classes
 contrasts <- makeContrasts(contrasts=phase,
@@ -103,6 +103,8 @@ fit <- contrasts.fit(fit, contrasts)
 
 # empirical Bayes smoothing to standard errors
 fit <- eBayes(fit)
+
+## sort by p-values (to be reviewed) ##
 toptable <- toptable(fit, sort.by='logFC', number=250, genelist = geneNames)
 toptable['gene'] <- geneNames[rownames(toptable)]
 
@@ -111,11 +113,10 @@ X.toptable <- X[rownames(toptable),]
 rownames(X.toptable) <- geneNames[rownames(toptable)]
 
 ######
-# Draw Graphs for the TopTable
+# Draw Graphs for DGEA
 ######
 
-# Heatmap
-
+# store Heatmap as an .svg file in the working directory
 filename <- paste(working.dir,"Heatmap.svg",sep = "")
 CairoSVG(file = filename)
 color_scale <- colorRampPalette(rev(brewer.pal(11, 'Spectral')))(100)
@@ -123,22 +124,21 @@ heatmap1 <- heatmap.2(X.toptable, col=color_scale, scale='row', key=T, keysize=1
                       dendrogram='row', density.info='none', trace='none', cexCol=0.5, cexRow=0.1)
 dev.off()
 
-# Histogram
+# store Histogram as an .svg file in the working directory
 filename <- paste(working.dir,"Histogram.svg",sep = "")
 CairoSVG(file = filename)
 hist(toptable$adj.P.Val, breaks=100, col='skyblue', border='slateblue', xlab = "Adjusted p-values")
 dev.off()
 
-# Volcano plot
+# store Volcano plot as an .svg file in the working directory
 filename <- paste(working.dir,"Volcano.svg",sep = "")
 CairoSVG(file = filename)
 volcanoplot(fit, coef=1, highlight=20, names=geneNames, col='steelblue', xlab='Log Fold Change',
             ylab='Log Odds', pch=16, cex=0.5)
 dev.off()
 
+# store Top20 genes as a .csv file in the working directory
 filename <- paste(working.dir,"TopGenes.csv",sep = "")
 write.csv(toptable[1:20,], file = filename)
-
-
 
 
