2f784ccc5e205ce70d1726cc6088e09c23c44643
MPriebe <m.k.priebe@se15.qmul.ac.uk>
2016-02-10 12:22:22 +0000

    Start loading data into pathview

--- pathview.R
+++ pathview.R
@@ -18,8 +18,8 @@ library("pathview")     # Creates Heatmap
 parser <- add_argument(parser, "--gagedata", 
                        help="The GAGE results full path")
 
-parser <- add_argument(parser, "--B.is.control",
-                       help="Boolean for whether Group B should be treated as a control")
+parser <- add_argument(parser, "--analysis.type",
+                       help="Genesets used and whether or not B should be treated as a control")
 
 parser <- add_argument(parser, "--geneset", 
                        help="The KEGG geneset code")
@@ -43,6 +43,53 @@ parser <- add_argument(parser, "--factor",
 argv <- parse_args(parser)
 
 
+#General parameters
+gagedata   <- argv$gagedata
+analysis.type <- argv$analysis.type
+factor.type <- argv$factor 
+population1     <- unlist(strsplit(argv$popA, ","))
+population2     <- unlist(strsplit(argv$popB, ","))
+
+
+
+#Loading and preparing data
+data <- load(gagedata)
+
+
+
+
+
+
+
+if(analysis.type =="KEGG_ExpVsCtrl"){
+  #Find expression change between experimental group and control
+  GEOdataset.d<-GEOdataset[, Group1] - rowMeans(GEOdataset[,Group2])
+  
+  ########## COMMON ##########
+  sel <- analysis$greater[, "q.val"] < 0.1 & !is.na(analysis$greater[, "q.val"])
+  path.ids <- rownames(analysis$greater)[sel]
+  path.ids2 <- substr(path.ids, 1, 8) 
+  ########## COMMON ##########
+  
+  ##Produces  top 3 interaction networks (from 2 way analysis)
+  pv.out.list <- sapply(path.ids2[1:3], function(pid) pathview(gene.data = GEOdataset.d[,1:2], pathway.id = pid, species = "hsa"))
+  
+}
+if(analysis.type =="KEGG_ExpVsExp"){
+  
+  ########## COMMON ##########
+  sel <- analysis$greater[, "q.val"] < 0.1 & !is.na(analysis$greater[, "q.val"])
+  path.ids <- rownames(analysis$greater)[sel]
+  path.ids2 <- substr(path.ids, 1, 8) 
+  ########## COMMON ##########
+  
+  ##Interaction pathways for experimental group 1
+  pv.out.list2 <- sapply(path.ids2[1:3], function(pid) pathview(gene.data = GEOdataset[,Group1names][,1:2], pathway.id = pid, species = "hsa"))
+  
+  ##Interaction pathways for experimental group 2
+  pv.out.list3 <- sapply(path.ids2[1:3], function(pid) pathview(gene.data = GEOdataset[,Group2names][,1:2], pathway.id = pid, species = "hsa"))
+  
+}
 
 
 
